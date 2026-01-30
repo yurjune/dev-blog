5e76cef08839cc450fc6c6c089d758fa10eb812b
yurjune <yurjune@naver.com>
2026-01-30 19:57:41 +0900

    feat: change font style of code block

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -49,7 +49,7 @@ export default function CodeBlock({ children, className }: CodeBlockProps) {
       )}
       <pre
         ref={preRef}
-        className={`${className} !mt-0 !mb-4 !text-sm !bg-neutral-800`}
+        className={`${className} !mt-0 !mb-4 !text-sm !bg-neutral-800 leading-relaxed`}
         suppressHydrationWarning
       >
         <code className={className}>{children}</code>
---
+++ src/lib/fonts.ts
@@ -0,0 +1,3 @@
+import { JetBrains_Mono } from "next/font/google";
+
+export const jetBrainsMono = JetBrains_Mono({ subsets: ["latin"] });
