77174da41b20b6ec293dc0d4560ebe5e40adcd5c
yurjune <yurjune@naver.com>
2026-03-01 20:30:27 +0900

    chore: make trailing slash false

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // output: "export", // 개발 중에는 주석 처리
-  trailingSlash: true,
+  trailingSlash: false,
   images: {
     unoptimized: true,
   },
